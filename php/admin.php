121274f1e7858649fed15bf85272bc5b3aa0d1fc
Unknown <legameur345@gmail.com>
2017-04-30 04:31:43 -0400

    list user - ajouter un utilisateur (form) - page confirmation

--- php/admin.php
+++ php/admin.php
@@ -27,7 +27,7 @@ global $bd;
 
 /*Connection a la DB*/
 try {
-  $bd = new PDO("mysql:host=localhost;dbname=webprojet", "root", "root");
+  $bd = new PDO('mysql:host=localhost;dbname=webprojet','root','root');
   /*activer les exeptions en cas d'erreur */
   $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
@@ -52,6 +52,8 @@ $varHTML .=
     "</ul>".PHP_EOL.
 "</div>".PHP_EOL;
 /*Générer le contenu selon où se situe l'administrateur*/
+
+/*Acceuil par défaut*/
 if (!isset($_GET['menu']))
 {
   $varHTML .=
@@ -61,69 +63,85 @@ if (!isset($_GET['menu']))
 }
 else
 {
+  /*SI ON CLIQUE SUR UN DES CHOIX DU MENU*/
  $varHTML .=
  "<div class='zoneContenu'>".PHP_EOL;
  switch ($_GET['menu'])
  {
-  case lst_user:
+  case 'lst_user':
       $varHTML .=
-          "<h2>Liste des utilisateurs</h2>".PHP_EOL.
-          "<p>Query à la DB,afficher dans un tableau</p>".PHP_EOL;
-          break;
-  case add_user:
+          "<h2>Liste des utilisateurs</h2>".PHP_EOL;
       $varHTML .=
-          "<h2>Ajouter un utilisateur</h2>".PHP_EOL.
-          "<p>Formulaire avec submit -->> doit également créer un user UNIX avec son quota
-          ,Processus de validation,DB double entry Unix entry</p>".PHP_EOL;
+          list_user($bd);
           break;
-  case mod_user:
+  case 'add_user':
+      $varHTML .=
+          "<h2>Ajouter un utilisateur</h2>".PHP_EOL;
+      if (!isset($_POST["submit1"]))
+      {
+      $varHTML .=
+        create_user($bd);
+        break;
+      }
+  case 'mod_user':
       $varHTML .=
           "<h2>Modifier un utilisateur</h2>".PHP_EOL.
           "<p>Formulaire avec submit,Processus de validation,DB alter</p>".PHP_EOL;
           break;
-  case pwd_chng:
+  case 'pwd_chng':
       $varHTML .=
           "<h2>Modifier un mot de passe</h2>".PHP_EOL.
           "<p>Formulaire avec submit,Processus de validation,DB entry</p>".PHP_EOL;
           break;
-  case pwd_hist:
+  case 'pwd_hist':
       $varHTML .=
           "<h2>Historique des changements de mot de passe</h2>".PHP_EOL.
           "<p>Query à la DB,afficher dans un tableau</p>".PHP_EOL;
           break;
-  case chk_syst:
+  case 'chk_syst':
       $varHTML .=
           "<h2>État du système</h2>".PHP_EOL.
           "<p>Trouver les Query nécessaires pour: Nom de la machine,
           Mémoire vive totale vs utilisée, Espace disque totale vs utilisé,
           état du processeur,afficher</p>".PHP_EOL;
           break;
-  case quo_gest:
+  case 'quo_gest':
       $varHTML .=
           "<h2>Gestion des quotas</h2>".PHP_EOL.
           "<p>Faire un premier formulaire avec 2 choix: 1. Voir les quotas
           -->>Query DB et Unix,afficher 2. Modifier le quota -->> Formulaire avec
           submit,Processus de validation,DB entry Unix entry</p>".PHP_EOL;
           break;
-  case clr_sess:
+  case 'clr_sess':
       $varHTML .=
-         "<h2>Confirmation de fermeture de session</h2>".PHP_EOL.
+         "<h2>Confirmation la fermeture de session</h2>".PHP_EOL.
          "<form action='./admin.php?menu=clr_sess' method='POST'>".PHP_EOL.
 			   "<input type='submit' name ='ctrl_nouvSess' width='50px' value='Quitter la session'</input>".PHP_EOL.
          "<input type='submit' name ='ctrl_backMain' width='50px' value='Retour'</input>".PHP_EOL.
 			   "</form>".PHP_EOL;
           break;
+  case 'conf_page':
+      $varHTML .=
+         "<h2>Confirmation des changements à la base de donnée</h2>".PHP_EOL.
+         "<form action='./admin.php?menu=conf_page' method='POST'>".PHP_EOL.
+         "<input type='submit' name ='ctrl_nouvSess' width='50px' value='Ajouter'</input>".PHP_EOL.
+         "<input type='submit' name ='ctrl_backMain' width='50px' value='Annuler'</input>".PHP_EOL.
+         "</form>".PHP_EOL;
+          break;
   default:
           break;
  }
+ $varHTML .=
+ "</div>".PHP_EOL;
+
 }
 
-if( isset($_POST['ctrl_backMain']))
+if(isset($_POST['ctrl_backMain']))
 {
   header('Location: ./admin.php');
 }
 
-if( isset($_POST['ctrl_nouvSess']))
+if(isset($_POST['ctrl_nouvSess']))
 {
   session_destroy();
   header('Location: ./index.php');
@@ -135,6 +153,7 @@ if( isset($_POST['ctrl_nouvSess']))
  <html>
    <head>
      <title>WEBPROJET</title>
+     <meta charset="UTF-8"/>
      <link rel="stylesheet" type="text/css" href="../css/styles.css">
    </head>
    <body>
--- php/functions.php
+++ php/functions.php
@@ -59,8 +59,129 @@ function authentification ($login, $password, $bd){
   }
   return $reponse;
 }
+/*===FONCTION FORMULAIRE NOUVEL UTILISATEUR===*/
 
+/*La fonction prend en paramètre la Connection à la base de donnée
+Elle retourne un formulaire POST en contenu DIV */
 
+function create_user ($bd)
+{
+  $contenuDiv = "";
+  $UserNumber = 0;
+  $DeptCount = 0;
+  $var_vect_Dept  = [];
+  $var_IndiceChamp = 0;
+  $var_i = 1;
+  $var_ValChamp    = "";
+  unset($_POST);
+
+  /*request 1. compte les user 2. compte les dept 3. liste des dept*/
+  $req_1 = $bd->query("SELECT * FROM Usagers_description");
+  $req_2 = $bd->query("SELECT nom_departement FROM Departements");
+
+  /*compte les user + 1 pour donner le user_ID*/
+  $UserNumber = ($req_1->rowCount())+1;
+  /*compte les dept pour la taille de la liste*/
+  $DeptCount = $req_2->rowCount();
+
+    $contenuDiv =
+    "<form method='POST' ACTION='./admin.php?menu=conf_page'>".PHP_EOL.
+      "<table cellpadding='10px'>".PHP_EOL.
+          "<tr>".PHP_EOL.
+            "<td><label for='fname'>Nom de famille</label>".PHP_EOL.
+            "<input type='text' id='fname' name='fname' maxlength='25'></input></td>".PHP_EOL.
+            "<td><label for='name'>Prénom</label>".PHP_EOL.
+            "<input type='text' id='name' name='name' maxlength='25'></input></td>".PHP_EOL.
+          "</tr>".PHP_EOL.
+          "<tr>".PHP_EOL.
+            "<td><label for='poste'># de poste téléphonique</label>".PHP_EOL.
+            "<input type='text' id='poste' name='poste' maxlength='4'></input></td>".PHP_EOL.
+            "<td><label for='machine'># de machine</label>".PHP_EOL.
+            "<input type='text' id='machine' name='machine' maxlength='4'></input></td>".PHP_EOL.
+          "</tr>".PHP_EOL.
+          "<tr>".PHP_EOL.
+            "<td><label for='newuser'>Nom d'utilisateur</label>".PHP_EOL.
+            "<input type='text' id='newuser' name='newuser' maxlength='12'></input></td>".PHP_EOL.
+            "<td><label for='tamere'>Mot de passe</label>".PHP_EOL.
+            "<input type='text' id='tamere' name='tamere' maxlength='12'></input></td>".PHP_EOL.
+          "</tr>".PHP_EOL.
+          "<tr>".PHP_EOL.
+            "<td><label for='dept'>Nom du département</label>".PHP_EOL;
+
+            //Construction dynamique de la liste des départements
+      $contenuDiv .=
+              "<select name='dept' id='dept' size=".$DeptCount."'>".PHP_EOL;
+            //Tant que tu as des départements, créer une option
+      while($var_vect_Dept = $req_2->fetch(PDO::FETCH_ASSOC))
+      {
+        foreach($var_vect_Dept as $var_IndiceChamp => $var_ValChamp)
+        {
+          $contenuDiv .=
+          "<option value='".($var_IndiceChamp+$var_i)."'>".$var_ValChamp."</option>".PHP_EOL;
+          $var_i++;
+        }
+      }
+          //fermeture du select et on termine le formulaire
+      $contenuDiv .=
+              "</select>".PHP_EOL.
+            "</td>".PHP_EOL.
+            "<td><label for='quot'>Taille du quota (Go)</label>".PHP_EOL.
+            "<input type='text' id='quot' name='quot' maxlength='2'></input></td>".PHP_EOL.
+          "</tr>".PHP_EOL.
+          "<tr>".PHP_EOL.
+            "<td align='right'>".PHP_EOL.
+            "<input type='reset' name='cancelForm' value='Annuler'></input></td>".PHP_EOL.
+            "<td>".PHP_EOL.
+            "<input type='Submit' name='submit1' value='Soumettre'></input></td>".PHP_EOL.
+          "</tr>".PHP_EOL.
+        "</table>".PHP_EOL.
+      "</form>".PHP_EOL;
+
+    return $contenuDiv;
+}
+/*===FONCTION LISTER LES UTILISATEUR===*/
+/*La fonction prend en paramètre la Connection à la base de donnée
+Elle retourne une liste en contenu DIV que si on clique sur un user
+on peut modifier ses informations (mod_user)*/
+
+function list_user ($bd)
+{
+
+  $contenuDiv = "";
+  $var_vect_User  = [];
+  $var_IndiceChamp = 0;
+  $var_i = 1;
+  $var_ValChamp    = "";
+
+  /*request pour grab tous les utilisateurs*/
+  $reqUser = $bd->query("SELECT CONCAT(prenom,' ',nom) FROM Usagers_description");
+
+  /*compte le nombre d'enregistrements*/
+  $count = $reqUser->rowCount();
+
+  if (!($count))
+  {
+    $contenuDiv = "<p>Aucun utilisateurs trouvés</p>";
+  }
+  else
+  {
+    /*start une liste*/
+    $contenuDiv = "<ul>".PHP_EOL;
+    /*tant que j'ai des user pour chaque user fait une liste */
+    while($var_vect_User = $reqUser->fetch(PDO::FETCH_ASSOC))
+    {
+      foreach($var_vect_User as $var_IndiceChamp => $var_ValChamp)
+      {
+        $contenuDiv .=
+        "<li><a href='./admin.php?menu=mod_user&user=".($var_IndiceChamp+$var_i)."'>".$var_ValChamp."</a></li>".PHP_EOL;
+        $var_i++;
+      }
+    }
+    /*ferme une liste*/
+    $contenuDiv .= "</ul>".PHP_EOL;
+  }
+  return $contenuDiv;
+}
 /*fonction quota*/
 /*function quotaUser (){
   $vartest = exec('whoiam');
